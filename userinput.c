08d29913f863dbff9c7d658e56f0768dabeba893
motty-dev <motzeltzer@gmail.com>
2023-01-24 01:19:55 +0200

    11. handle input function

---
+++ userinput.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include "userinput.h"
+#include "list.h"
+#include "utils.h"
+
+#define INPUT_BUFFER_SIZE 256
+#define ARR_LEN(_arr) (sizeof(_arr)/sizeof(_arr[0]))
+
+
+void handle_input(Customer *head, char *file_name) 
+{
+    char input[INPUT_BUFFER_SIZE];
+    char command[16], *parameters=NULL;
+    char *cursor;
+
+    // get input from the user
+    printf("Enter a command { > select >set >print >quit }\n > ");
+    fgets(input, INPUT_BUFFER_SIZE, stdin);
+    input[strcspn(input, "\n")] = 0;
+    cursor = input;
+
+    // extract the command
+    int len = strcspn(cursor, " ");
+    strncpy(command, cursor, len);
+    command[len] = '\0';
+    len++;
+
+    // move the cursor to the start of the parameters
+    cursor += len;
+    cursor += strspn(cursor, "\n");
+
+    // extract the parameters
+    len = strlen(cursor);
+    parameters = (char*)malloc(len);    //len +1
+    strncpy(parameters, cursor, len);
+    parameters[len] = '\0';
+
+    if (strcmp(command, "select") == 0) {
+        handle_select(parameters, head);
+    } else if (strcmp(command, "set") == 0) {
+        handle_set(parameters, &head, file_name);
+    } else if (strcmp(command, "print") == 0) {
+        print_list(head);
+    } else if (strcmp(command, "quit") == 0) {
+        exit(0);
+    } else {
+        printf("Error: '%s' is not a valid command. Allowed commands are select, set, print, quit.\n", command);
+    }
+    printf("parameters:%s\n", parameters);
+    free(parameters);
+}
