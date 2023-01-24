e47a27e643c8b5dfc57ed90337173cc80f013a7a
motty-dev <motzeltzer@gmail.com>
2023-01-24 22:00:09 +0200

    28. handle_set 4th version

--- userinput.c
+++ userinput.c
@@ -54,6 +54,170 @@ void print_customer(Customer *c)
 void handle_set(char *input, Customer **head, char *file_name) 
 {
     char *token;
+    Customer new_customer = {0};
+    int fields_count = 0;
+    int missing_fields = 0;
+
+    if(input == NULL || input[0] == '\0')
+    {
+       printf("Error: Empty Input\n");
+       return;
+    }
+
+    missing_fields = check_missing_fields(input);
+    if (missing_fields > 0) {
+        printf("Error: Missing %d required fields.\n", missing_fields);
+        return;
+    }
+
+    while ((token = strtok_r(input, ",", &input)) != NULL) {
+        if (!handle_field(token, "", &new_customer)) {
+            return;
+        }
+        fields_count++;
+    }
+
+    if (fields_count != 6) {
+        printf("\nError: expected 6 fields, but got %d .\nCheck that you fill up all the fields\n {first name=,last name=,id=,phone=,date=,debt=}\n" , fields_count);
+        return;
+    }
+
+    if(!validate_first_name(new_customer.first_name)){
+        printf("Wrong input value for first name field.\n");
+        return;
+    }
+    if(!validate_last_name(new_customer.last_name)){
+        printf("Wrong input value for last name field.\n");
+        return;
+    }
+    if(!validate_telephone(new_customer.phone)){
+        printf("Wrong input value for phone field.\n");
+        return;
+    }
+    if(!validate_date(new_customer.date)){
+        printf("Wrong input value for date field.\n");
+        return;
+    }
+    if(!validate_id_number(new_customer.id_number)){
+        printf("Wrong input value for id field.\n");
+        return;
+    }
+    // add new customer to the list logic( after missing fields check )
+    Customer *temp = *head;
+    Customer *prev = NULL;
+    int found = 0;
+    while (temp != NULL) {
+        if (strcasecmp(temp->id_number, new_customer.id_number) == 0) {
+            found = 1;
+            if(strcasecmp(temp->first_name, new_customer.first_name) != 0 || strcasecmp(temp->last_name, new_customer.last_name) != 0){
+                printf("This id already exist for a different customer.\n");
+                return;
+            }else if((strcasecmp(temp->phone, new_customer.phone) != 0)){
+                //save_to_csv(&new_customer, file_name);
+                strcpy(temp->phone, new_customer.phone);
+                printf("User exist with different phone. debt was updated in the list and added to file.");
+            }
+            float temp_s = temp->debt;
+            temp->debt += new_customer.debt;
+            if (compare_date(new_customer.date, ">", temp->date))
+            {
+                strcpy(temp->date, new_customer.date);
+            }
+            if (prev == NULL) {
+                *head = temp->next;
+            } else {
+                prev->next = temp->next;
+            }
+            save_to_csv(&new_customer, file_name);
+            insert_in_order(temp, head);
+            printf("Debt was updated for customer: %s %s, from %.2f to %.2f\n", temp->first_name, temp->last_name, temp_s, temp->debt);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    if (!found) {
+        build_list(new_customer, head);
+        save_to_csv(&new_customer, file_name);
+        printf("Added new customer to the list!\n");
+    }
+}
+
+void handle_select(char *parameters, Customer *head) 
+{
+    char field[32], operator[4], value[64];
+    int ret = 1;
+    extract_field_operator_value(parameters, field, operator, value, &ret);
+    if (ret)
+    {   
+        int match_found = 0;
+        Customer *current = head;
+        while (current != NULL) {
+            if (match_customer(current, field, operator, value)) {
+                match_found++;
+                print_customer(current);
+            }
+            current = current->next;
+        }
+        printf("%d results found.\n",match_found);
+        if (!match_found) {
+            printf("No match found\n");
+        }
+    }  
+}
+
+void handle_input(Customer *head, char *file_name) 
+{
+    char input[INPUT_BUFFER_SIZE];
+    char command[16], *parameters=NULL;
+    char *cursor;
+
+    // get input from the user
+    printf("Enter a command { > select >set >print >quit }\n > ");
+    fgets(input, INPUT_BUFFER_SIZE, stdin);
+    input[strcspn(input, "\n")] = 0;
+    cursor = input;
+
+    // extract the command
+    int len = strcspn(cursor, " ");
+    strncpy(command, cursor, len);
+    command[len] = '\0';
+    len++;
+
+    // move the cursor to the start of the parameters
+    cursor += len;
+    cursor += strspn(cursor, "\n");
+
+    // extract the parameters
+    len = strlen(cursor);
+    parameters = (char*)malloc(len);    //len +1
+    strncpy(parameters, cursor, len);
+    parameters[len] = '\0';
+
+    if (strcmp(command, "select") == 0) {
+        handle_select(parameters, head);
+    } else if (strcmp(command, "set") == 0) {
+        handle_set(parameters, &head, file_name);
+    } else if (strcmp(command, "print") == 0) {
+        print_list(head);
+    } else if (strcmp(command, "quit") == 0) {
+        exit(0);
+    } else {
+        printf("Error: '%s' is not a valid command. Allowed commands are select, set, print, quit.\n", command);
+    }
+    printf("parameters:%s\n", parameters);
+    free(parameters);
+}
+
+
+
+
+
+
+/*
+void handle_set(char *input, Customer **head, char *file_name) 
+{
+    char *token;
     Customer new_customer ={0};
     int fields_count = 0;
     int first_name_set, last_name_set, id_set, phone_set, debt_set, date_set = 0;
@@ -195,69 +359,4 @@ void handle_set(char *input, Customer **head, char *file_name)
         printf("Added new customer to the list!");
     }  
 }
-
-void handle_select(char *parameters, Customer *head) 
-{
-    char field[32], operator[4], value[64];
-    int ret = 1;
-    extract_field_operator_value(parameters, field, operator, value, &ret);
-    if (ret)
-    {   
-        int match_found = 0;
-        Customer *current = head;
-        while (current != NULL) {
-            if (match_customer(current, field, operator, value)) {
-                match_found++;
-                print_customer(current);
-            }
-            current = current->next;
-        }
-        printf("%d results found.\n",match_found);
-        if (!match_found) {
-            printf("No customer matching the query\n");
-        }
-    }  
-}
-
-void handle_input(Customer *head, char *file_name) 
-{
-    char input[INPUT_BUFFER_SIZE];
-    char command[16], *parameters=NULL;
-    char *cursor;
-
-    // get input from the user
-    printf("Enter a command { > select >set >print >quit }\n > ");
-    fgets(input, INPUT_BUFFER_SIZE, stdin);
-    input[strcspn(input, "\n")] = 0;
-    cursor = input;
-
-    // extract the command
-    int len = strcspn(cursor, " ");
-    strncpy(command, cursor, len);
-    command[len] = '\0';
-    len++;
-
-    // move the cursor to the start of the parameters
-    cursor += len;
-    cursor += strspn(cursor, "\n");
-
-    // extract the parameters
-    len = strlen(cursor);
-    parameters = (char*)malloc(len);    //len +1
-    strncpy(parameters, cursor, len);
-    parameters[len] = '\0';
-
-    if (strcmp(command, "select") == 0) {
-        handle_select(parameters, head);
-    } else if (strcmp(command, "set") == 0) {
-        handle_set(parameters, &head, file_name);
-    } else if (strcmp(command, "print") == 0) {
-        print_list(head);
-    } else if (strcmp(command, "quit") == 0) {
-        exit(0);
-    } else {
-        printf("Error: '%s' is not a valid command. Allowed commands are select, set, print, quit.\n", command);
-    }
-    printf("parameters:%s\n", parameters);
-    free(parameters);
-}
+*/
